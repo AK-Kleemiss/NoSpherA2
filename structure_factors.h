f866d196b85855ff93b8d22f2efff60e9305d80a
Florian Kleemiß <florian.kleemiss@uni-bremen.de>
2019-11-01 17:14:33 +0100

    updated structure factor calculation

--- structure_factors.h
+++ structure_factors.h
@@ -7,7 +7,7 @@
 
 class WFN;
 
-bool calculate_structure_factors(std::string &hkl_filename, std::string &cif_file, WFN &wave, bool debug, int accuracy, std::ofstream &file);
+bool calculate_structure_factors(std::string &hkl_filename, std::string &cif_file, std::string &symm, WFN &wave, bool debug, int accuracy, std::ofstream &file);
 
 #include "wfn_class.h"
 
