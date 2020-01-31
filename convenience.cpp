d93fbac9aa5a69e3f035c6a02879ecdb8442f8fc
Florian Kleemiß <florian.kleemiss@uni-bremen.de>
2020-01-31 12:08:23 +0100

    fixes for update

--- convenience.cpp
+++ convenience.cpp
@@ -1001,3 +1001,20 @@ bool unsaved_files(vector<WFN> &wavy){
 	return false;
 };
 
+
+void progress_bar::write(double fraction)
+{
+	// clamp fraction to valid range [0,1]
+	if (fraction < 0)
+		fraction = 0;
+	else if (fraction > 1)
+		fraction = 1;
+
+	auto width = bar_width - message.size();
+	auto offset = bar_width - static_cast<unsigned>(width * fraction);
+
+	os << '\r' << message;
+	os.write(full_bar.data() + offset, width);
+	os << " [" << std::setw(3) << static_cast<int>(100 * fraction) << "%] " << std::flush;
+}
+
--- convenience.h
+++ convenience.h
@@ -70,6 +70,48 @@ void select_cubes(std::vector <std::vector <unsigned int> > &selection, std::vec
 bool unsaved_files(std::vector<WFN> &wavy);
 int get_Z_from_label(const char * tmp);
 
+//-------------------------Progress_bar--------------------------------------------------
+
+class progress_bar
+{
+	static const auto overhead = sizeof " [100%]";
+
+	std::ostream& os;
+	const std::size_t bar_width;
+	std::string message;
+	const std::string full_bar;
+
+public:
+	progress_bar(std::ostream& os, std::size_t line_width,
+		std::string message_, const char symbol = '=')
+		: os{ os },
+		bar_width{ line_width - overhead },
+		message{ std::move(message_) },
+		full_bar{ std::string(bar_width, symbol) + std::string(bar_width, ' ') }
+	{
+		if (message.size() + 1 >= bar_width || message.find('\n') != message.npos) {
+			os << message << '\n';
+			message.clear();
+		}
+		else {
+			message += ' ';
+		}
+		write(0.0);
+	}
+
+	// not copyable
+	progress_bar(const progress_bar&) = delete;
+	progress_bar& operator=(const progress_bar&) = delete;
+
+	~progress_bar()
+	{
+		write(1.0);
+		os << '\n';
+	}
+
+	void write(double fraction);
+};
+
 #include "wfn_class.h"
 
 #endif
