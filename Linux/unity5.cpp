284a5a10d5a2e803934212c287607bcbb5ab7d1a
FlorianKleemiss <florian.kleemiss@uni-bremen.de>
2020-06-26 10:48:09 +0200

    update of makefiles

--- Linux/unity5.cpp
+++ Linux/unity5.cpp
@@ -1,7 +1,7 @@
-#include "/home/florian/wfn2fchk/AtomGrid.cpp"
+#include "../AtomGrid.cpp"
 
 
-#include "/home/florian/wfn2fchk/basis_set.cpp"
+#include "../basis_set.cpp"
 
 
-#include "/home/florian/wfn2fchk/becke_partitioning.cpp"
+#include "../becke_partitioning.cpp"
