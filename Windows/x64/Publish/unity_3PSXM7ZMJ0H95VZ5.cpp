5fa49815ed01600ef3666ef3ee96ba49bdd6463b
Florian Kleemiß <florian.kleemiss@uni-bremen.de>
2020-07-31 11:23:02 +0200

    update

---
+++ Windows/x64/Publish/unity_3PSXM7ZMJ0H95VZ5.cpp
@@ -0,0 +1,3 @@
+
+#include "D:\git\wfn2fchk\properties.cpp"
+
---
+++ Windows/x64/Publish/unity_ER3SXB5O3191F1GP.cpp
@@ -0,0 +1,9 @@
+
+#include "D:\git\wfn2fchk\grid_radial.cpp"
+
+
+#include "D:\git\wfn2fchk\mo_class.cpp"
+
+
+#include "D:\git\wfn2fchk\sphere_lebedev_rule.cpp"
+
