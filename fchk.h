b7cb240139220a51833c907a35a69b328d25aee4
Florian Kleemiß <florian.kleemiss@arcor.de>
2022-06-29 10:35:13 +0200

    finalized version of gbw reader and cleaned up code without Enter()

--- fchk.h
+++ fchk.h
@@ -4,10 +4,9 @@
 
 class WFN;
 
-std::string prepare_gaussian(const std::string &basis_set_path, const std::string &fchkname, 
-                     WFN &wave, const int &ncpus, const float &mem, bool debug);
-bool modify_fchk(const std::string &fchk_name, const std::string &basis_set_path, WFN &wave, bool &debug, const bool &read);
-bool free_fchk(const std::string &fchk_name, const std::string &basis_set_path, WFN &wave, bool &debug, bool force_overwrite = false);
+//std::string prepare_gaussian(const std::string& basis_set_path, const std::string& fchkname,
+//  WFN& wave, const int& ncpus, const float& mem, bool debug);
+bool modify_fchk(const std::string& fchk_name, const std::string& basis_set_path, WFN& wave, bool& debug, const bool& read);
 bool free_fchk(std::ofstream& file, const std::string& fchk_name, const std::string& basis_set_path, WFN& wave, bool& debug, bool force_overwrite = false);
 
 #include "wfn_class.h"
