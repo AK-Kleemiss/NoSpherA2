98ffa65a35adf9e7223b4825d5171337b37afb7b
Florian Kleemiß <florian.kleemiss@arcor.de>
2023-01-19 13:01:04 +0100

    include DLL files

---
+++ NoSpherA2_DLL/framework.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+#include "../convenience.h"
+#include "../fchk.h"
+#include "../cube.h"
+#include "../basis_set.h"
+#include "../structure_factors.h"
+#include "../properties.h"
+#include "../cell.h"
+#include "../spherical_density.h"
