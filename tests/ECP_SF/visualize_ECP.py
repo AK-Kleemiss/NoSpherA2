010e6be616524b2b0e2801a1f75f7a1ae1ae9da9
Florian Kleemiß <florian.kleemiss@gmail.com>
2024-02-08 16:24:16 +0100

    working

--- tests/ECP_SF/visualize_ECP.py
+++ tests/ECP_SF/visualize_ECP.py
@@ -19,25 +19,26 @@ lf = os.path.join(dir, 'sfacs.dat')
 # 4 = ORCA ECP FF
 df = pd.read_csv(lf, delim_whitespace=True, header=None)
 
-# Create a figure with 3x2 subplots
+plt.rcParams['lines.linestyle'] = ''
+plt.rcParams['lines.marker'] = '.'
+plt.rcParams['lines.markersize'] = 0.5
+# Create a figure with 2x2 subplots
 fig, axs = plt.subplots(2,2, figsize=(12, 8))
 
 axs[0][0].plot(df[0], df[1], label='Thakkar')
+axs[0][0].plot(df[0], df[4], label='ORCA Full')
 axs[0][0].legend()
 
-axs[0][1].plot(df[0], df[2], label='Thakkar Core')
-axs[0][1].legend()
-
-axs[1][0].plot(df[0], df[3], label='ORCA Valence')
-axs[1][0].legend()
-
-axs[1][1].plot(df[0], df[4], label='ORCA Full')
+axs[1][1].plot(df[0], df[1]-df[2] - df[3], label='Thakkar - ORCA Valence')
 axs[1][1].legend()
 
+axs[0][1].plot(df[0], df[1]-df[2], label = 'Thakkar - Thakkar Core')
+axs[0][1].plot(df[0], df[3], label='ORCA Valence')
+axs[0][1].legend()
 
-# Show the figure
-#plt.show()
-#exit(0)
+axs[1][0].plot(df[0], df[1]-df[4], label = 'Thakkar - ORCA')
+axs[1][0].legend()
+
 fig.savefig("ECP_SF_Au.png", dpi=300, bbox_inches='tight')
 exit(0)
 
