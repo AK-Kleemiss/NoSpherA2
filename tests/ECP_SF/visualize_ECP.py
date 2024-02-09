ce230b30bc5d948c2d3d22c553fc36182eb4c9c7
Florian Kleemiß <florian.kleemiss@gmail.com>
2024-02-09 19:44:48 +0100

    finalized all electron plots

--- tests/ECP_SF/visualize_ECP.py
+++ tests/ECP_SF/visualize_ECP.py
@@ -3,8 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir)
-a0 = 0.52917721067
-
 lf = os.path.join(dir, 'sfacs.dat')
 # Read the file
 # 0 = k
@@ -18,9 +16,7 @@ lf = os.path.join(dir, 'sfacs.dat')
 # 8 = ORCA_all Valence FF
 df = pd.read_csv(lf, delim_whitespace=True, header=None)
 
-plt.rcParams['lines.linestyle'] = ''
-plt.rcParams['lines.marker'] = '.'
-plt.rcParams['lines.markersize'] = 0.5
+plt.rcParams['lines.linewidth'] = 0.3
 # Create a figure with 2x2 subplots
 fig, axs = plt.subplots(2,2, figsize=(12, 8))
 
@@ -42,12 +38,10 @@ axs[0][1].set_title('Valence SF')
 axs[0][1].legend()
 
 axs[1][0].plot(df[0], df[1]-df[4], label = 'Thakkar - HAR')
+axs[1][0].plot(df[0], df[7]-df[4], label = 'ORCA Jorge - HAR')
 axs[1][0].plot(df[0], df[7]-df[1], label = 'ORCA Jorge - Thakkar')
 axs[1][0].plot(df[0], df[6]-df[1], label = 'ORCA def2 - Thakkar')
 axs[1][0].plot(df[0], df[7]-df[6], label = 'ORCA Jorge - ORCA def2')
-axs[1][0].plot(df[0], df[7]-df[4], label = 'ORCA Jorge - HAR')
 axs[1][0].legend()
 
-fig.savefig("ECP_SF_Au.png", dpi=300, bbox_inches='tight')
-exit(0)
-
+fig.savefig("ECP_SF_Au.png", dpi=300, bbox_inches='tight') 
