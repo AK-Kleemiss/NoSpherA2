8021028e2bdc25c1857e61e45c6fe66ffefc6b12
Florian Kleemiß <florian.kleemiss@gmail.com>
2024-07-12 07:00:53 +0200

    getting rid of some warnings

--- Src/SALTED_math.h
+++ Src/SALTED_math.h
@@ -5,17 +5,29 @@
 
 struct Shape2D
 {
-    int rows;
-    int cols;
+    size_t rows;
+    size_t cols;
+    Shape2D(size_t rows, size_t cols) : rows(rows), cols(cols) {}
+    Shape2D() : rows(0), cols(0) {}
+    Shape2D(int rows, int cols) : rows(rows), cols(cols) {
+      err_checkf(rows >= 0, "cannot have negative size!", std::cout);
+      err_checkf(cols >= 0, "cannot have negative cols!", std::cout);
+    }
 };
 
 struct Shape3D
 {
-    int depth;
-    int rows;
-    int cols;
+    size_t depth;
+    size_t rows;
+    size_t cols;
+    Shape3D(size_t depth, size_t rows, size_t cols) : depth(depth), rows(rows), cols(cols) {}
+    Shape3D() : depth(0), rows(0), cols(0) {}
+    Shape3D(int depth, int rows, int cols) : depth(depth), rows(rows), cols(cols) { 
+      err_checkf(depth >= 0, "cannot have negative size!", std::cout);
+      err_checkf(rows >= 0, "cannot have negative size!", std::cout);
+      err_checkf(cols >= 0, "cannot have negative size!", std::cout);
+    }
 };
-
 
 // To_2D
 template <typename T>
@@ -27,43 +39,43 @@ std::vector<std::vector<std::vector<T>>> reshape(std::vector<T> flatVec, Shape3D
 // TRANSPOSES
 // 3D MATRIX
 template <typename T>
-std::vector<std::vector<std::vector<T>>> transpose(const std::vector<std::vector<std::vector<T>>>& originalVec);
+std::vector<std::vector<std::vector<T>>> transpose(const std::vector<std::vector<std::vector<T>>> &originalVec);
 
 // 2D MATRIX
 template <class T>
-std::vector<std::vector<T>> transpose(const std::vector<std::vector<T>>& mat);
+std::vector<std::vector<T>> transpose(const std::vector<std::vector<T>> &mat);
 
 // Reorder 3D std::vectors following a given order
 template <typename T>
-std::vector<std::vector<std::vector<T>>> reorder3D(const std::vector<std::vector<std::vector<T>>>& original);
+std::vector<std::vector<std::vector<T>>> reorder3D(const std::vector<std::vector<std::vector<T>>> &original);
 
 // Function to collect rows from a vec2 based on a vector of indices
-vec2 collectRows(const vec2& matrix, const std::vector<int>& indices);
+vec2 collectRows(const vec2 &matrix, const std::vector<int> &indices);
 
 // Function to collect rows from a vec3 based on a vector of indices
-vec3 collectRows(const vec3& cube, const std::vector<int>& indices);
+vec3 collectRows(const vec3 &cube, const std::vector<int> &indices);
 
-//FLATTEN Operation
-// Flatten vector 2D
+// FLATTEN Operation
+//  Flatten vector 2D
 template <typename T>
-std::vector<T> flatten(const std::vector<std::vector<T>>& vec2D);
+std::vector<T> flatten(const std::vector<std::vector<T>> &vec2D);
 // Flatten vector 3D
 template <typename T>
-std::vector<T> flatten(const std::vector<std::vector<std::vector<T>>>& vec3D);
+std::vector<T> flatten(const std::vector<std::vector<std::vector<T>>> &vec3D);
 
 // SLICE Operation
-std::vector<double> slice(const std::vector<double>& vec, size_t start, size_t length);
+std::vector<double> slice(const std::vector<double> &vec, size_t start, size_t length);
 
 // Matrix multiplication
 // 2D x 2D MATRIX MULTIPLICATION
 template <typename T>
-std::vector<std::vector<T>> dot(const std::vector<std::vector<T>>& mat1, const std::vector<std::vector<T>>& mat2);
+std::vector<std::vector<T>> dot(const std::vector<std::vector<T>> &mat1, const std::vector<std::vector<T>> &mat2);
 
 // 2D x 1D MATRIX MULTIPLICATION
 template <typename T>
-std::vector<T> dot(const std::vector<std::vector<T>>& mat, const std::vector<T>& vec);
+std::vector<T> dot(const std::vector<std::vector<T>> &mat, const std::vector<T> &vec);
 
-//Element-wise exponentiation of a matrix
-vec2 elementWiseExponentiation(const vec2& matrix, double exponent);
+// Element-wise exponentiation of a matrix
+vec2 elementWiseExponentiation(const vec2 &matrix, double exponent);
 
 void test_dot();
