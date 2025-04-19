bfff17cba295041f753df9d105e7b9f80caed2f5
Florian Kleemiß <florian.kleemiss@arcor.de>
2025-04-19 23:05:41 +0200

    Update AtomGrid.cpp

--- Src/AtomGrid.cpp
+++ Src/AtomGrid.cpp
@@ -440,6 +440,7 @@ const double get_r_inner(const double& max_error, const double& alpha_inner)
   return r;
 }
 
+#pragma GCC optimize("-fno-fast-math") //gcc fails to converge in this function when using fast math, so not use it
 // TCA 106, 178 (2001), eq. 19
 double get_r_outer(const double& max_error,
   const double& alpha_outer,
@@ -508,3 +509,4 @@ double get_h(const double& max_error, const int& l, const double& guess)
 
   return h;
 }
+#pragma GCC optimize("fast-math")
