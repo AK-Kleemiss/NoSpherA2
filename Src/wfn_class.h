84238c1b473f76fe490703c57599867c0ecd2615
Florian Kleemiß <florian.kleemiss@gmail.com>
2025-06-13 22:30:15 +0200

    Fix cartesian type order comment (#27)

--- Src/wfn_class.h
+++ Src/wfn_class.h
@@ -34,7 +34,7 @@ private:
     // 1                                            = S,
     // 2,3,4                                        = X,Y,Z
     // 5,6,7,8,9,10                                 = XX, YY, ZZ, XY, XZ, YZ
-    // 11,12,13,14,15,16,17,118,19,20               = XXX, YYY, ZZZ, XXY, XXZ, YYZ, XYY, XZZ, YZZ, XYZ etc..
+    // 11,12,13,14,15,16,17,18,19,20                = XXX, YYY, ZZZ, XXY, XXZ, YYZ, XYY, XZZ, YZZ, XYZ etc..
     // 21,22,23,24,25,26,27,28,29,30,31,32,33,34,35 = XXXX YYYY ZZZZ XXXY XXXZ XYYY YYYZ XZZZ YZZZ XXYY XXZZ YYZZ XXYZ XYYZ XYZZ
     //                                                0     1     2   3     4   5     6   7     8   9     10  11  12    13    14
     // for sphericals the order is:
