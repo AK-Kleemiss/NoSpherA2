f994c35599bb865707cb8ea2cf69bd438286c824
Florian Kleemiß <florian.kleemiss@gmail.com>
2026-08-14 07:50:28 +0200

    Update convenience.h

--- Src/core/convenience.h
+++ Src/core/convenience.h
@@ -732,7 +732,7 @@ struct options
     bool no_date = false;
     bool gbw2wfn = false;
     bool old_tsc = false;
-    bool label_tsc_output = true;
+    bool label_tsc_output = false;
     bool write_CIF = false;
     bool test = false;
     bool electron_diffraction = false;
